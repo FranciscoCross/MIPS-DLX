29086ec98ba8109a6dd05160a8e1fa41447fc039
matialmar <malmarcha@plantium.com>
2023-04-04 17:17:08 -0300

    Improved baudrategen

--- MIPS_DLX.srcs/sources_1/new/UART/baudrategen.v
+++ MIPS_DLX.srcs/sources_1/new/UART/baudrategen.v
@@ -1,24 +1,30 @@
-`timescale 10ns / 1ps //10ns es el semi ciclo, un ciclo entero tiene 20ns osea 50MHz
-module baudrategen
+`timescale 1ns / 1ps
+
+`define N_TICKS 16
+
+module baudrategen 
     #(
-        parameter N_BITS = 8,
-        parameter N_COUNT = 163 //Se espera que cada 163 ciclos de clock generar una señal tick para muestrear
-    )                           //El valor 163 es para un baudrate de 19200 bauds y un clock de 50MHz (50*10^6 / 19200*16)
-    (
+        parameter CLK = 50e6, 
+        parameter BAUD_RATE = 9600
+    )(
         input wire clock,
         input wire reset,
         output wire tick
     );
 
-    reg [N_BITS-1:0] count;
-    wire reset_counter = (count == N_COUNT-1) ? 1'b1 : 1'b0;
+    localparam integer N_BCLK_DIV = (CLK / (BAUD_RATE*`N_TICKS));
+    localparam N_BITS = $clog2 (N_BCLK_DIV);
+
+    reg [N_BITS - 1 : 0] count;
+    wire reset_counter = (count == N_BCLK_DIV) ? 1'b1 : 1'b0;
 
     always @(posedge clock)
     begin
         if(reset) count <= 0;
-        else if (reset_counter) count <= 0;
-        else count = count + 1;        
+        else if(reset_counter) count <= 0;
+        else count = count + 1;
     end
 
     assign tick = reset_counter;
+
 endmodule
