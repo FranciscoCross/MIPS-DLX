a1bd68fc56d228d4836831d6a95dd50874cde838
matialmar7 <matias.almarcha@mi.unc.edu.ar>
2023-05-29 23:19:19 -0300

    Updated memory intialization

--- MIPS_DLX.srcs/sources_1/new/dmem.v
+++ MIPS_DLX.srcs/sources_1/new/dmem.v
@@ -18,19 +18,21 @@ module dmem
    
     output wire [NB_DATA-1:0] o_data
   );
-
+  
     reg [NB_DATA-1:0] RAM[`N_ELEMENTS-1:0];
     reg [NB_DATA-1:0] data_reg = {NB_DATA{1'b0}};
 
     assign o_data = data_reg;
     //Para Inicializar la memoria en cero
-    initial
-    begin
-      for (integer i = 0; i < `N_ELEMENTS; i = i + 1)
-          RAM[i] <= {NB_DATA{1'b0}};
-    end
-
-
+    generate
+      integer i;		
+      initial 
+      begin
+        RAM[0] = {NB_DATA{1'b0}};
+        for (i = 1; i < 128; i = i + 1)
+            RAM[i] = {NB_DATA{1'b0}}; 
+      end
+    endgenerate
     
   always @(posedge i_clk)
     begin
