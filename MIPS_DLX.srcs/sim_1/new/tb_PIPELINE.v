5f25b1bcd9de0f31dfb0e5a0b6c49e6a5117a156
FraciscoCross <franciscoo.delacruzz@gmail.com>
2023-06-29 00:19:47 -0300

    add instruccions for tb pipeline

--- MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
+++ MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
@@ -17,6 +17,8 @@ module tb_PIPELINE;
   reg i_ctrl_read_debug_reg;
   reg i_ctrl_wr_debug_mem;
   reg i_ctrl_addr_debug_mem;
+  // Declaración del arreglo de instrucciones
+  reg [31:0] instrucciones [0:10];
 
   // Outputs
   wire o_bit_sucio;
@@ -25,6 +27,7 @@ module tb_PIPELINE;
   wire [31:0] o_data_mem_debug_unit;
   wire [7:0] o_count_cycles;
   wire o_halt;
+  integer i;
 
   // Instantiate the pipeline module
   pipeline pipeline (
@@ -50,6 +53,33 @@ module tb_PIPELINE;
     .o_halt(o_halt)
   );
 
+  // Proceso para cargar las instrucciones en el arreglo
+
+  always @(posedge clock) begin
+    if (i_reset) 
+      begin
+        // Reiniciar el índice de carga
+        i_addr_inst_load <= 0;
+        i_inst_load <= 0;
+        i <= 0;
+      end 
+      else if (i_en_write && i < 2) 
+        begin
+        // Cargar instrucciones en el arreglo
+          if (i_debug_unit) 
+            begin
+              i_inst_load = instrucciones[i];
+              i_addr_inst_load = i_addr_inst_load+ 1;  
+              i = i + 1;
+            end 
+          else 
+            begin
+              i_addr_inst_load <= i_addr_inst_load;
+              i_inst_load <= i_inst_load;
+            end
+
+        end
+  end
   // Clock generation
   always #1 clock = ~clock;
 
@@ -69,13 +99,51 @@ module tb_PIPELINE;
     i_ctrl_read_debug_reg = 0;
     i_ctrl_wr_debug_mem = 0;
     i_ctrl_addr_debug_mem = 0;
+    instrucciones[0] = 32'b10001100010001110000000000000000;  // LW $7, 0($2) //100011  00010  00111  0000000000000000
+    //Los primeros 6 bits (opcode) indican que es una instrucción de carga (opcode = 100011).
+    //Los siguientes 5 bits (rs) especifican el registro de origen rs, que en este caso es $2 (rs = 00010 en binario).
+    //Los siguientes 5 bits (rt) especifican el registro de destino rt, que en este caso es $1 (rt = 00111 en binario).
+    //Los últimos 16 bits (offset) representan el desplazamiento (offset) de 16 bits para acceder a la dirección de memoria, que en este caso es 0 (offset = 0000000000000000 en binario).
+    instrucciones[1] = 32'b00000000001000100010000000100001;  
+    // ADD $4, $1, $2 //000000 00001 00010 00100 00000 100001
+    //Los primeros    6 bits (opcode) indican que es una instrucción de tipo R (opcode = 000000).
+    //Los siguientes  5 bits (rs) especifican el registro de origen rs, que en este caso es $1 (rs = 00001 en binario).
+    //Los siguientes  5 bits (rt) especifican el segundo registro de origen rt, que en este caso es $2 (rt = 00010 en binario).
+    //Los siguientes  5 bits (rd) especifican el registro de destino rd, que en este caso es $4 (rd = 00100 en binario).
+    //Los siguientes  5 bits (shamt) se reservan para el desplazamiento, que es cero en este caso (shamt = 00000 en binario).
+    //Los últimos     6 bits (funct) contienen el código de función específico de la operación "ADD", que es 100000 en binario.
+    
+    // instrucciones[1] = 32'h00000002;
+    // instrucciones[2] = 32'h00000003;
+    // instrucciones[3] = 32'h00000004;
+    // instrucciones[4] = 32'h00000005;
+    // instrucciones[5] = 32'h00000006;
+    // instrucciones[6] = 32'h00000007;
+    // instrucciones[7] = 32'h00000008;
+    // instrucciones[8] = 32'h00000009;
+    // instrucciones[9] = 32'h0000000A;
+    
 
-    #10 i_reset = 1; // Apply reset
-    #20 i_reset = 0; // Deassert reset
-
+    #2 i_reset = 1; // Apply reset
+    #2 i_reset = 0; // Deassert reset
+    #2
+    i_debug_unit = 1;
+    i_en_write = 1;
+    #2
+    i_debug_unit = 0;
+    i_en_write = 0;
+    #20
+    i_enable_pipe = 1;
     // TODO: Provide test stimulus here
 
     #100 $finish; // End simulation
   end
 
 endmodule
+
+
+
+
+
+
+
