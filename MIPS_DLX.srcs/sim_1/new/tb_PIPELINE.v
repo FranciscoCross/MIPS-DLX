f7a2297dece1355e8c3bf4d0fd197d680bc7414b
FraciscoCross <franciscoo.delacruzz@gmail.com>
2023-06-30 17:34:14 -0300

    test ADD tb Pipeline

--- MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
+++ MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
@@ -100,19 +100,21 @@ module tb_PIPELINE;
     i_ctrl_wr_debug_mem = 0;
     i_ctrl_addr_debug_mem = 0;
     instrucciones[0] = 32'b11111000000000000000000000000000;  // NOP
-    instrucciones[1] = 32'b10001100010001110000000000000000;  // LW $7, 0($2) //100011  00010  00111  0000000000000000 //8C470000
+    //LW COMMENT
+    //instrucciones[1] = 32'b10001100010001110000000000000000;  // LW $7, 0($2) //100011  00010  00111  0000000000000000 //8C470000
     //Los primeros 6 bits (opcode) indican que es una instrucción de carga (opcode = 100011).
     //Los siguientes 5 bits (rs) especifican el registro de origen rs, que en este caso es $2 (rs = 00010 en binario).//2
     //Los siguientes 5 bits (rt) especifican el registro de destino rt, que en este caso es $1 (rt = 00111 en binario).//7
-    //Los últimos 16 bits (offset) representan el desplazamiento (offset) de 16 bits para acceder a la dirección de memoria, que en este caso es 0 (offset = 0000000000000000 en binario).
-    instrucciones[2] = 32'b00000000001000100010000000100001;  
-    // ADD $4, $1, $2 //000000 00001 00010 00100 00000 100001
+    //Los ultimos 16 bits (offset) representan el desplazamiento (offset) de 16 bits para acceder a la dirección de memoria, que en este caso es 0 (offset = 0000000000000000 en binario).
+    //ADD COMMENT
+    instrucciones[1] = 32'b00000000001001010010000000100001;  
+    // ADD $4, $1, $5 //000000 00001 00101 00100 00000 100001
     //Los primeros    6 bits (opcode) indican que es una instrucción de tipo R (opcode = 000000).
     //Los siguientes  5 bits (rs) especifican el registro de origen rs, que en este caso es $1 (rs = 00001 en binario).
-    //Los siguientes  5 bits (rt) especifican el segundo registro de origen rt, que en este caso es $2 (rt = 00010 en binario).
+    //Los siguientes  5 bits (rt) especifican el segundo registro de origen rt, que en este caso es $2 (rt = 00101 en binario).
     //Los siguientes  5 bits (rd) especifican el registro de destino rd, que en este caso es $4 (rd = 00100 en binario).
     //Los siguientes  5 bits (shamt) se reservan para el desplazamiento, que es cero en este caso (shamt = 00000 en binario).
-    //Los últimos     6 bits (funct) contienen el código de función específico de la operación "ADD", que es 100000 en binario.
+    //Los ultimos     6 bits (funct) contienen el código de función específico de la operación "ADD", que es 100001 en binario.
     
     // instrucciones[1] = 32'h00000002;
     // instrucciones[2] = 32'h00000003;
