f2660dd683a20487db858ad9959159b46f6356d7
matialmar7 <matias.almarcha@mi.unc.edu.ar>
2023-07-06 20:03:03 -0300

    last changes

--- MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
+++ MIPS_DLX.srcs/sim_1/new/tb_PIPELINE.v
@@ -74,48 +74,14 @@ module tb_PIPELINE;
     i_ctrl_wr_debug_mem = 0;
     i_ctrl_addr_debug_mem = 0;
  
-    
-    //instrucciones[0] = 32'b11111000000000000000000000000000;  // NOP
-    //instrucciones[1] = 32'b10001100000000010000000000000000; //lw R1, 0(0)
-    //instrucciones[2] = 32'b10001100000000100000000000000001; //lw R2, 1(0)
-    //instrucciones[3] = 32'b00000000010000010001100000100001; //addu R3, R2, R1
-    //instrucciones[4] = 32'b10101100000000110000000000000010; //sw R3, 2(0)
-    //instrucciones[5] = 32'b11111100000000000000000000000000; //halt
-
     #2 i_reset = 1; // Apply reset
     #2 i_reset = 0; // Deassert reset
-    
-    
+  
     //CARGAR UNA INSTRUCCION
     #2
     i_debug_unit = 1;
     #2
-    
-     //LW COMMENT
-    //i_inst_load = 32'b10001100010001110000000000000000;  // LW $7, 0($2) //100011  00010  00111  0000000000000000 //8C470000
-    //Los primeros 6 bits (opcode) indican que es una instrucción de carga (opcode = 100011).
-    //Los siguientes 5 bits (rs) especifican el registro de origen rs, que en este caso es $2 (rs = 00010 en binario).//2
-    //Los siguientes 5 bits (rt) especifican el registro de destino rt, que en este caso es $1 (rt = 00111 en binario).//7
-    //Los ultimos 16 bits (offset) representan el desplazamiento (offset) de 16 bits para acceder a la dirección de memoria, que en este caso es 0 (offset = 0000000000000000 en binario).
-   
-    //#2
-    //i_en_write = 1;
-    //#2
-    //i_en_write = 0;
-    //i_inst_load = 0;
-    //#2
-    //i_addr_inst_load = i_addr_inst_load + 1;    
-    
-        
-     //ADD COMMENT
-    i_inst_load = 32'b00000000001001010010000000100001;  
-    // ADD $4, $1, $5 //000000 00001 00101 00100 00000 100001
-    //Los primeros    6 bits (opcode) indican que es una instrucción de tipo R (opcode = 000000).
-    //Los siguientes  5 bits (rs) especifican el registro de origen rs, que en este caso es $1 (rs = 00001 en binario).
-    //Los siguientes  5 bits (rt) especifican el segundo registro de origen rt, que en este caso es $2 (rt = 00101 en binario).
-    //Los siguientes  5 bits (rd) especifican el registro de destino rd, que en este caso es $4 (rd = 00100 en binario).
-    //Los siguientes  5 bits (shamt) se reservan para el desplazamiento, que es cero en este caso (shamt = 00000 en binario).
-    //Los ultimos     6 bits (funct) contienen el código de función específico de la operación "ADD", que es 100001 en binario.
+    i_inst_load = 32'b10001100010001010000000000000000;  // LW $5, 0($2) //100011  00010  00111  0000000000000000 //8C470000
     #2
     i_en_write = 1;
     #2
@@ -123,9 +89,16 @@ module tb_PIPELINE;
     i_inst_load = 0;
     #2
     i_addr_inst_load = i_addr_inst_load + 1;
-    i_debug_unit = 0;
-    
-    
+    #2    
+    i_inst_load = 32'b00000000010001010010000000100001; //ADD $4, $2, $5 
+    #2
+    i_en_write = 1;
+    #2
+    i_en_write = 0;
+    i_inst_load = 0;
+    #2
+    i_addr_inst_load = i_addr_inst_load + 1;
+    i_debug_unit = 0;   
     #3 // 2 x n instrucciones
     i_enable_pipe = 1;
     i_en_read = 1;
